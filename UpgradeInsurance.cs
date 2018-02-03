f4bd9d1dd5e9250ba9ceaa63086d456e599698fc
Lan949 <36101921+Lan949@users.noreply.github.com>
2018-02-03 19:58:54 +0800

    Update UpgradeInsurance.cs

--- UpgradeInsurance.cs
+++ UpgradeInsurance.cs
@@ -1,1 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace AOOADproject
+{
+    class UpgradeInsurance : InsurancePolicy
+    {
+        public UpgradeInsurance(int a, double b, double c, DateTime d, DateTime e, string f, string g)
+        {
+            PolicyNo = a;
+            Cost = b;
+            Payout = c;
+            PolicyStartDate = d;
+            PolicyEndDate = e;
+            PaymentType = f;
+            Status = g;
+
+        }
+    }
+}
